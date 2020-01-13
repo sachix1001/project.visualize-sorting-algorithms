32d7903a77b0ca7e9599c9e13a0ffd527a33342d
Sachi Grannan <sachix1001@gmail.com>
2020-01-13 16:45:36 +0900

    refactor sort

--- src/Sort.js
+++ src/Sort.js
@@ -2,42 +2,43 @@ class SomeKindOfSort {
   constructor(array) {
     this.array = array;
   }
+  heapify(length, index) {
+    let biggest = index;
+    let left = 2 * index + 1;
+    let right = left + 1;
+
+    if (left < length && this.array[biggest] < this.array[left]) {
+      biggest = left;
+    }
+
+    if (right < length && this.array[biggest] < this.array[right]) {
+      biggest = right;
+    }
+
+    if (biggest != index) {
+      [this.array[biggest], this.array[index]] = [
+        this.array[index],
+        this.array[biggest],
+      ];
+      this.heapify(length, biggest);
+    }
+  }
   sort() {
     let length = this.array.length;
     let index = Math.floor(length / 2 - 1);
     let last = length - 1;
 
-    const heapify = (length, index) => {
-      let biggest = index;
-      let left = 2 * index + 1;
-      let right = left + 1;
-      if (left < length && this.array[biggest] < this.array[left]) {
-        biggest = left;
-      }
-      if (right < length && this.array[biggest] < this.array[right]) {
-        biggest = right;
-      }
-
-      if (biggest != index) {
-        [this.array[biggest], this.array[index]] = [
-          this.array[index],
-          this.array[biggest],
-        ];
-        heapify(length, biggest);
-      }
-      return this.array;
-    };
-
     while (index >= 0) {
-      heapify(length, index);
+      this.heapify(length, index);
       index--;
     }
 
     while (last >= 0) {
-      [this.array[0], this.array[last]] = [this.array[last], this.array[0]];
-      heapify(last, 0);
+      [this.array[last], this.array[0]] = [this.array[0], this.array[last]];
+      this.heapify(last, 0);
       last--;
     }
+
     return this.array;
   }
 
@@ -47,4 +48,3 @@ class SomeKindOfSort {
 }
 
 module.exports = SomeKindOfSort;
-// [ 8, 6, 7, 4, 5, 3, 2, 1 ]
