a266aa83e3a06d2f390e4709720295b1f57c6048
Sachi Grannan <sachix1001@gmail.com>
2020-01-13 16:17:36 +0900

    complete sort method

--- src/Sort.js
+++ src/Sort.js
@@ -3,24 +3,42 @@ class SomeKindOfSort {
     this.array = array;
   }
   sort() {
-    function heapify(arr, length, index) {
+    let length = this.array.length;
+    let index = Math.floor(length / 2 - 1);
+    let last = length - 1;
+
+    const heapify = (length, index) => {
       let biggest = index;
       let left = 2 * index + 1;
       let right = left + 1;
-
-      if (arr[biggest] < arr[left]) {
+      if (left < length && this.array[biggest] < this.array[left]) {
         biggest = left;
-        let temp = arr[biggest];
-        arr[biggest] = arr[left];
-        arr[left] = temp;
       }
-      if (arr[biggest] < arr[right]) {
+      if (right < length && this.array[biggest] < this.array[right]) {
         biggest = right;
-        let temp = arr[biggest];
-        arr[biggest] = arr[right];
-        arr[right] = temp;
       }
+
+      if (biggest != index) {
+        [this.array[biggest], this.array[index]] = [
+          this.array[index],
+          this.array[biggest],
+        ];
+        heapify(length, biggest);
+      }
+      return this.array;
+    };
+
+    while (index >= 0) {
+      heapify(length, index);
+      index--;
     }
+
+    while (last >= 0) {
+      [this.array[0], this.array[last]] = [this.array[last], this.array[0]];
+      heapify(last, 0);
+      last--;
+    }
+    return this.array;
   }
 
   returnValue(value) {
@@ -29,3 +47,4 @@ class SomeKindOfSort {
 }
 
 module.exports = SomeKindOfSort;
+// [ 8, 6, 7, 4, 5, 3, 2, 1 ]
--- test/_sort.js
+++ test/_sort.js
@@ -11,6 +11,6 @@ describe("Sort", () => {
   it("should return a sorted array", () => {
     const input = [6, 5, 3, 1, 8, 7, 2, 4];
     const array = new Sort(input);
-    expect(array.sort()).to.be([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(array.sort()).to.eql([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
