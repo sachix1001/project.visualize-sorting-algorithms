06327f211866fd40372321c0e22efe3d0222ce9b
Sachi Grannan <sachix1001@gmail.com>
2020-01-13 15:20:28 +0900

    add heapify function inside sort

--- src/Sort.js
+++ src/Sort.js
@@ -3,7 +3,24 @@ class SomeKindOfSort {
     this.array = array;
   }
   sort() {
-    console.log("sort!");
+    function heapify(arr, length, index) {
+      let biggest = index;
+      let left = 2 * index + 1;
+      let right = left + 1;
+
+      if (arr[biggest] < arr[left]) {
+        biggest = left;
+        let temp = arr[biggest];
+        arr[biggest] = arr[left];
+        arr[left] = temp;
+      }
+      if (arr[biggest] < arr[right]) {
+        biggest = right;
+        let temp = arr[biggest];
+        arr[biggest] = arr[right];
+        arr[right] = temp;
+      }
+    }
   }
 
   returnValue(value) {
