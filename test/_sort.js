4778ca76f93e1271567f11afb500df7f473622ba
Sachi Grannan <sachix1001@gmail.com>
2020-01-13 15:19:58 +0900

    add one test

--- test/_sort.js
+++ test/_sort.js
@@ -8,4 +8,9 @@ describe("Sort", () => {
   it("should have a sort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
   });
+  it("should return a sorted array", () => {
+    const input = [6, 5, 3, 1, 8, 7, 2, 4];
+    const array = new Sort(input);
+    expect(array.sort()).to.be([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
 });
